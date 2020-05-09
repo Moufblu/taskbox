c46845647c9875ab3277454565286a29137fe961
Nadir Benallal <nadir.benallal@heig-vd.ch>
2020-05-09 17:07:58 +0200

    Added long title test story

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -22,6 +22,8 @@ export const taskData = {
 
 const taskTemplate = `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`;
 
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 // default task state
 export const Default = () => ({
   components: { Task },
@@ -56,6 +58,20 @@ export const Archived = () => ({
       default: () => ({
         ...taskData,
         state: 'TASK_ARCHIVED',
+      }),
+    },
+  },
+  methods: actionsData,
+});
+
+export const LongTitle = () => ({
+  components: { Task },
+  template: taskTemplate,
+  props: {
+    task: {
+      default: () => ({
+        ...taskData,
+        title: longTitle,
       }),
     },
   },
